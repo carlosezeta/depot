d316f779e10d27093a58aa5ba59e1c4f30e6ff98
Carlos Rodriguez <carlos.ezetateam@gmail.com>
2014-04-18 21:35:12 +0200

    Añadido carrito de la compra
    Añadido el carrito, basado en la sesión del usuario y creado el test
    correspondiente.

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,7 +1,19 @@
 class Product < ActiveRecord::Base
   attr_accessible :description, :image_url, :price, :title
-  
   default_scope :order => 'title'
+  has_many :line_items
+
+  before_destroy :ensure_not_referenced_by_any_line_item
+
+  # comprobar que no hay pedidos referentes a este producto
+  def ensure_not_referenced_by_any_line_item
+  	if line_items.count.zero?
+  		return true
+  	else
+  		errors[:base] << "Line items present"
+  		return false
+  	end
+  end
 
   validates :title, :description, :image_url, :presence => true
   validates :price, :numericality => {:greater_than_or_equal_to => 0.01}
